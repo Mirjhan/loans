8c06b42efd852bffc200b713d5e6fcdd7cce2df9
Mirjhan <mirjhanmoreyra@gmail.com>
2026-09-02 22:22:09 -0500

    feat: test create a conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:loans/src/login/ui/pages/login_page_2.dart';
+import 'package:loans/src/login/ui/pages/quick_login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: const LoginPage2(),
+      home: const RecentLoginPage(),
     );
   }
 }
